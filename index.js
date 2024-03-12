d458a001773af67e4429d6069db19f63072001d8
Nitesh Yaduvanshi <niteshkumar@dal.ca>
2024-03-12 13:59:29 -0300

    Validation & conditions

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-let firstCard = 4
-let secondCard = 6
+//Initiating cards & conditional variables
+let firstCard = 10
+let secondCard = 4
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
-console.log(`${sum}`)
+
+// Game condition
+if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂"
+} else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackJack = true
+} else {
+    message = "You're out of the game! 😭"
+    isAlive = false
+}
+
+
+console.log(message)
